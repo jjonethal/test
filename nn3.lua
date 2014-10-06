7e980178b02f191da5d13b670bb2364bc12533e0
Jean Jonethal <jjonethal@gmail.com>
2014-10-06 23:11:28 +0200

    initial version

---
+++ nn3.lua
@@ -0,0 +1,50 @@
+-- nn3.lua
+local exp=math.exp
+BIAS = -1
+
+-- sigmoid activation function
+function sigmoid(a)
+	return 1.0/(1.0 + exp(-a))
+end
+
+function calcNeuron(neuron, inp)
+	local a = 0
+	for i=1,#neuron-1 do
+		a = a + neuron[i] * inp[i]
+		print("a",a)
+	end
+	a = a + neuron[#neuron] * BIAS
+	return sigmoid(a)
+end
+
+function calcLayer(layer, input)
+	local o={}
+	for k,n in ipairs(layer) do
+		o[k]=calcNeuron()
+	end
+	return o
+end
+
+function copy(t1)
+	local t2={}
+	for i,v in ipairs(t1) do
+		t2[i]=v
+	end
+	return t2
+end
+
+function testCalcNeuron()
+	local inp    = {0.1,.1,.1,.1}
+	local neuron = {1,   1,  1,  1,  1}
+	print("calcNeuron",calcNeuron(neuron, inp))
+end
+
+
+function testSigmoid()
+	for i=-10,10 do
+		print(i,sigmoid(i))
+	end
+end
+
+testCalcNeuron()
+
